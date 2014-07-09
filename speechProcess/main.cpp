13b19dc26ee75a930089d7ffc9d272371f2a83f4
cyburgee <collin.e.burger@gmail.com>
2014-07-08 21:18:37 -0700

    Initial Commit

---
+++ speechProcess/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  speechProcess
+//
+//  Created by Collin Burger on 7/8/14.
+//
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
